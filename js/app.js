fd2030f609ee9a18f4ff9686a3a68b74a0a79458
Meenakshi Sharma <mee2208sharma@gmail.com>
2021-06-17 16:30:55 +0530

    Deleted Notes

--- js/app.js
+++ js/app.js
@@ -30,18 +30,21 @@ function reloadUserNotes(){
     let html="";
     let notesParentElement=document.getElementById('myNotes');
 
-    if(storedNotes && storedNotes.length>0){
+    if( storedNotes && storedNotes.length>0){
     noteObject.forEach((element,index) => {
         html+=`<div class="noteCard card my-2 mx-2" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title">Note ${index+1}</h5>
           <p class="card-text">${element}</p>
-          <a href="#" class="btn btn-primary">Delete Note</a>
+          <button class="btn btn-primary" id="${index}" onclick="deleteNote(this.id)" >Delete Note</button>
         </div>
       </div>`;
     });
 
-    
+    //on deleting all node, storedNodes was not empty but hmtl length is till 0
+    if(html.length==0){
+         html=`Nothing to show, Create Notes now! :) `;
+        }
     notesParentElement.innerHTML=html;
 }
 else{
@@ -54,3 +57,18 @@ else{
 window.onload=(event)=>{
     reloadUserNotes();
 };
+
+//function to delete a note
+function deleteNote(nodeIndex){
+    let storedNotes = localStorage.getItem("notes");
+    if(storedNotes){
+        noteObject=JSON.parse(storedNotes);
+    }
+    else{
+        noteObject=[];
+
+    }
+    noteObject.splice(nodeIndex,1); // delete 1 item from index
+    localStorage.setItem("notes",JSON.stringify(noteObject));
+    reloadUserNotes();
+}
