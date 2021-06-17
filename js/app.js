c20879d6277bd9f44e8eb8277af3d91736c48a70
Meenakshi Sharma <mee2208sharma@gmail.com>
2021-06-17 20:20:49 +0530

    Feature add:DeleteAll, MarkImportant

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
 
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click',(e)=>{
-    let newTextEntered = document.getElementById('addText').value;
-    let newTextEnteredTitle = document.getElementById('addTitle').value;
+    let newTextEntered = document.getElementById('addText');
+    let newTextEnteredTitle = document.getElementById('addTitle');
     let storedNotes = localStorage.getItem("notes");
     if(storedNotes){
         noteObject=JSON.parse(storedNotes);
@@ -12,9 +12,11 @@ addBtn.addEventListener('click',(e)=>{
         noteObject=[];
 
     }
-    noteObject.push({'Title':newTextEnteredTitle,'Data':newTextEntered});
+    noteObject.push({'Title':newTextEnteredTitle.value,'Data':newTextEntered.value,'Imp':false});
     localStorage.setItem("notes",JSON.stringify(noteObject));
     newTextEntered.value="";
+    newTextEnteredTitle.value="";
+
     reloadUserNotes();
 });
 
@@ -33,14 +35,35 @@ function reloadUserNotes(){
 
     if( storedNotes && storedNotes.length>0){
     noteObject.forEach((element,index) => {
+        if(element.Imp==true){
+            html+=`<div class="noteCard card my-2 mx-2" style="width: 18rem;background-color: darksalmon;">
+            <i class="fa fa-star my-4" style="color:oldlace;align-self: flex-end;" onclick="markImportant(${index})"></i>
         
+            <div class="card-body">
+              <h5 class="card-title">Note ${element.Title}</h5>
+              
+              <p class="card-text">${element.Data}</p>
+    
+              <button class="btn btn-secondary" id="${index}" onclick="deleteNote(this.id)" >Delete Note</button>
+            
+             
+            </div>
+          </div>`;
+        }
+        else{
         html+=`<div class="noteCard card my-2 mx-2" style="width: 18rem;">
+        <i class="fa fa-star my-4" style="color:red;align-self: flex-end;" onclick="markImportant(${index})"></i>
+    
         <div class="card-body">
           <h5 class="card-title">Note ${element.Title}</h5>
+          
           <p class="card-text">${element.Data}</p>
-          <button class="btn btn-primary" id="${index}" onclick="deleteNote(this.id)" >Delete Note</button>
+
+          <button class="btn btn-secondary" id="${index}" onclick="deleteNote(this.id)" >Delete Note</button>
+        
+         
         </div>
-      </div>`;
+      </div>`;}
     });
 
     //on deleting all node, storedNodes was not empty but hmtl length is till 0
@@ -90,3 +113,40 @@ searchBar.addEventListener('input',(e)=>{
         }
    })
 });
+
+//to makr important not
+function markImportant(elementIndex){
+    
+    let storedNotes = localStorage.getItem("notes");
+    if(storedNotes){
+        noteObject=JSON.parse(storedNotes);
+    }
+    else{
+        noteObject=[];
+
+    }
+    let tempImportantNote=noteObject[elementIndex];
+    tempImportantNote.Imp=!tempImportantNote.Imp;//mark the item important
+    
+    noteObject.splice(elementIndex,1); // delete 1 item from localStorage
+
+    if(tempImportantNote.Imp==true){
+    noteObject.unshift(tempImportantNote); //add the item at the starting as it is important
+    }
+
+    else{
+        noteObject.push(tempImportantNote); // if it is marked not important push it at end
+    }
+
+
+
+    localStorage.setItem("notes",JSON.stringify(noteObject));
+    reloadUserNotes();
+
+}
+
+//delete all data storage
+function deleteAllNotes(){
+    localStorage.clear();
+    reloadUserNotes();
+}
